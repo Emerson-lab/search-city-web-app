40ec481005517c39fa4d00632280304234ca7c4e
Emerson Trindade <trinddade29@gmail.com>
2022-12-13 20:32:01 -0300

    feat: Added clear theme and provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
+import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>gg</p>} />
-
-
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            Button
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   )
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#834781",
+      dark: "#6B3267",
+      // light:,
+      contrastText: "#fff",
+    },
+    background: {
+      paper: "#fff",
+      default: "#f7f6f3"
+    }
+  }
+})
