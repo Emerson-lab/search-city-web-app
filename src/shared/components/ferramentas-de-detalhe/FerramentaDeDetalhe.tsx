1c3bc078ffbc115accb3533fde6ba87111c2ed4f
Emerson Trindade <trinddade29@gmail.com>
2023-01-12 20:52:07 -0300

    toolbar buttons

---
+++ src/shared/components/ferramentas-de-detalhe/FerramentaDeDetalhe.tsx
@@ -0,0 +1,64 @@
+import { Box, Button, Divider, Icon, Paper, useTheme } from "@mui/material";
+
+export const FerramentaDeDetalhe: React.FC = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      height={theme.spacing(5)}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display="flex"
+      alignItems="center"
+      gap={1}
+      component={Paper}
+    >
+      <Button
+        variant="contained"
+        color="primary"
+        disableElevation
+        startIcon={<Icon>save</Icon>}
+      >
+        Salvar
+      </Button>
+      <Button
+        variant="outlined"
+        color="primary"
+        disableElevation
+        startIcon={<Icon>save</Icon>}
+      >
+        Salvar e voltar
+      </Button>
+      <Button
+        variant="outlined"
+        color="primary"
+        disableElevation
+        startIcon={<Icon>delete</Icon>}
+      >
+        Apagar
+      </Button>
+      <Button
+        variant="outlined"
+        color="primary"
+        disableElevation
+        startIcon={<Icon>add</Icon>}
+      >
+        Novo
+      </Button>
+
+      <Divider
+        variant='middle'
+        orientation="vertical"
+      />
+
+      <Button
+        variant="outlined"
+        color="primary"
+        disableElevation
+        startIcon={<Icon>arrow_back</Icon>}
+      >
+        Voltar
+      </Button>
+    </Box>
+  )
+}
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,4 @@
 export * from './ferramentas-da-listagem/FerramentasDaListagem';
+export * from './ferramentas-de-detalhe/FerramentaDeDetalhe';
 export * from './menu-lateral/MenuLateral';
 
