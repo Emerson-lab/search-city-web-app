1297719a50f558cb2b6f715205c9d85874ef4fa2
Emerson Trindade <trinddade29@gmail.com>
2023-01-20 17:34:08 -0300

    hook: useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -2,26 +2,30 @@ import { useEffect, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import { FerramentasDaListagem } from "../../shared/components";
+import { useDebounce } from "../../shared/hooks";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { PessoasServices } from "../../shared/services/api/pessoas/PessoasServices";
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce();
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasServices.getAll(1, busca)
-      .then((result) => {
-        if (result instanceof Error) {
-          alert(result.message);
-        } else {
-          console.log('[result]', result);
-        }
-      })
-  }, [busca])
+    debounce(() => {
+      PessoasServices.getAll(1, busca)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log('[result]', result);
+          }
+        })
+    })
+  }, [busca]);
 
   return (
     <LayoutBaseDePagina
